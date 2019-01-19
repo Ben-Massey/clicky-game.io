1e531fe21a84a33fb2570420d21f82b99cd1c1d0
Ben-Massey <pojoben@gmail.com>
2019-01-19 16:36:38 -0600

    Updates

---
+++ precache-manifest.387a966517b188ff9ce5153092026624.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "02835e24be81d3dc2619",
+    "url": "/clicky-game.io/static/js/runtime~main.02835e24.js"
+  },
+  {
+    "revision": "5ca0fe1119d8bac9efb3",
+    "url": "/clicky-game.io/static/js/main.5ca0fe11.chunk.js"
+  },
+  {
+    "revision": "a00183a1ada18006ff08",
+    "url": "/clicky-game.io/static/js/1.a00183a1.chunk.js"
+  },
+  {
+    "revision": "5ca0fe1119d8bac9efb3",
+    "url": "/clicky-game.io/static/css/main.1a0a465b.chunk.css"
+  },
+  {
+    "revision": "18e88ffd9ee60582fa743c4ef0e52e27",
+    "url": "/clicky-game.io/index.html"
+  }
+];
